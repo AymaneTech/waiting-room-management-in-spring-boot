95332f6102519be0c856e509ed3152fda4a462c2
geek <elmainiaymane03@gmail.com>
2024-11-11 22:56:45 +0100

    use default configuration values in code

--- src/main/java/com/wora/waitingRoom/waitingList/application/service/impl/DefaultWaitingListService.java
+++ src/main/java/com/wora/waitingRoom/waitingList/application/service/impl/DefaultWaitingListService.java
@@ -1,37 +1,39 @@
 package com.wora.waitingRoom.waitingList.application.service.impl;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.wora.waitingRoom.waitingList.application.dto.request.WaitingListRequestDto;
-import com.wora.waitingRoom.waitingList.domain.valueObject.Algorithm;
-import com.wora.waitingRoom.waitingList.domain.valueObject.Mode;
-import jakarta.transaction.Transactional;
-
 import com.wora.waitingRoom.common.domain.exception.EntityNotFoundException;
+import com.wora.waitingRoom.waitingList.application.dto.request.WaitingListRequestDto;
 import com.wora.waitingRoom.waitingList.application.dto.response.WaitingListResponseDto;
 import com.wora.waitingRoom.waitingList.application.mapper.WaitingListMapper;
 import com.wora.waitingRoom.waitingList.application.service.WaitingListService;
 import com.wora.waitingRoom.waitingList.domain.entity.WaitingList;
 import com.wora.waitingRoom.waitingList.domain.repository.WaitingListRepository;
+import com.wora.waitingRoom.waitingList.domain.valueObject.Algorithm;
+import com.wora.waitingRoom.waitingList.domain.valueObject.Mode;
 import com.wora.waitingRoom.waitingList.domain.valueObject.WaitingListId;
-
-import org.springdoc.core.converters.ResponseSupportConverter;
+import com.wora.waitingRoom.waitingList.infrastructure.WaitingListConfigurationValues;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.Optional;
 
 import static com.wora.waitingRoom.common.util.OptionalWrapper.orElseThrow;
 
 @Service
 @Transactional
 @Validated
-@RequiredArgsConstructor
 public class DefaultWaitingListService implements WaitingListService {
     private final WaitingListRepository repository;
     private final WaitingListMapper mapper;
-    private final ResponseSupportConverter responseSupportConverter;
+    private final WaitingListConfigurationValues configurationProperties;
+
+    public DefaultWaitingListService(WaitingListRepository repository, WaitingListMapper mapper, WaitingListConfigurationValues configurationProperties) {
+        this.repository = repository;
+        this.mapper = mapper;
+        this.configurationProperties = configurationProperties;
+    }
 
     @Override
     public List<WaitingListResponseDto> findAll() {
@@ -48,20 +50,20 @@ public class DefaultWaitingListService implements WaitingListService {
 
     @Override
     public WaitingListResponseDto create(WaitingListRequestDto dto) {
+        System.out.println(configurationProperties);
         WaitingList waitingList = mapper.toEntity(dto);
 
         Optional.ofNullable(dto.capacity())
                 .ifPresentOrElse(waitingList::setCapacity,
-                        () -> waitingList.setCapacity(9393));
+                        () -> waitingList.setCapacity(configurationProperties.capacity()));
 
         Optional.ofNullable(dto.mode())
                 .ifPresentOrElse(waitingList::setMode,
-                        () -> waitingList.setMode(Mode.PART_TIME));
+                        () -> waitingList.setMode(configurationProperties.mode()));
 
         Optional.ofNullable(dto.algorithm())
-            .ifPresentOrElse(
-                    waitingList::setAlgorithm,
-                    () -> waitingList.setAlgorithm(Algorithm.FIFO));
+                .ifPresentOrElse(waitingList::setAlgorithm,
+                        () -> waitingList.setAlgorithm(configurationProperties.algorithm()));
 
         WaitingList savedWaitingList = repository.save(waitingList);
         return mapper.toResponseDto(savedWaitingList);
@@ -82,7 +84,7 @@ public class DefaultWaitingListService implements WaitingListService {
     @Override
     public void delete(WaitingListId id) {
         if (!repository.existsById(id))
-            throw new EntityNotFoundException("waiting list",  id.value());
+            throw new EntityNotFoundException("waiting list", id.value());
 
         repository.deleteById(id);
     }
