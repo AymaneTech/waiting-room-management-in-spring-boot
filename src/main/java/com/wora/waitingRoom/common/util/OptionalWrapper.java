05581bee6fa9a136e3416c60f339a4a1a388fc92
geek <elmainiaymane03@gmail.com>
2024-11-11 22:19:26 +0100

    add other orElseThrow helper

--- src/main/java/com/wora/waitingRoom/common/util/OptionalWrapper.java
+++ src/main/java/com/wora/waitingRoom/common/util/OptionalWrapper.java
@@ -1,12 +1,16 @@
 package com.wora.waitingRoom.common.util;
 
-import com.wora.waitingRoom.common.domain.exception.EntityNotFoundException;
-
 import java.util.Optional;
+
+import com.wora.waitingRoom.common.domain.exception.EntityNotFoundException;
 
 public class OptionalWrapper {
     public static <T> T orElseThrow(Optional<T> optional, String entity, Object id) {
         return optional.orElseThrow(() -> new EntityNotFoundException(entity, id));
     }
-
+    
+    public static <T> T orElseThrow(Optional<T> optional, String message) {
+        return optional.orElseThrow(() -> new EntityNotFoundException(message));
+    }
+    
 }
