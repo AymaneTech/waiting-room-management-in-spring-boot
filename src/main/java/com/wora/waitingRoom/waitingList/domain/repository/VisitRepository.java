3f7f6ff6ff5fcd3654c16894eee712768515eb73
geek <elmainiaymane03@gmail.com>
2024-11-11 21:53:03 +0100

    add visit repository

--- src/main/java/com/wora/waitingRoom/waitingList/domain/repository/VisitRepository.java
+++ src/main/java/com/wora/waitingRoom/waitingList/domain/repository/VisitRepository.java
@@ -1,9 +1,9 @@
 package com.wora.waitingRoom.waitingList.domain.repository;
 
-import com.wora.waitingRoom.waitingList.domain.entity.WaitingList;
-import com.wora.waitingRoom.waitingList.domain.valueObject.WaitingListId;
+import com.wora.waitingRoom.waitingList.domain.entity.Visit;
+import com.wora.waitingRoom.waitingList.domain.valueObject.VisitId;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WaitingListRepository extends JpaRepository<WaitingList, WaitingListId>{
+public interface VisitRepository extends JpaRepository<Visit, VisitId>{
 }
