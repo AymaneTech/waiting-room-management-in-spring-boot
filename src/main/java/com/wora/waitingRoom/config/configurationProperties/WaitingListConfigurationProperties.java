8a33db3577d2fb074b978ec6ee650b1e2686eb17
geek <elmainiaymane03@gmail.com>
2024-11-12 09:43:44 +0100

    make the configuration properties of open api and waiting list defaults have defautl values

--- src/main/java/com/wora/waitingRoom/config/configurationProperties/WaitingListConfigurationProperties.java
+++ src/main/java/com/wora/waitingRoom/config/configurationProperties/WaitingListConfigurationProperties.java
@@ -3,11 +3,17 @@ package com.wora.waitingRoom.config.configurationProperties;
 import com.wora.waitingRoom.waitingList.domain.valueObject.Algorithm;
 import com.wora.waitingRoom.waitingList.domain.valueObject.Mode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "app.waiting-list.default")
 public record WaitingListConfigurationProperties(
+        @DefaultValue("15")
         Integer capacity,
+
+        @DefaultValue("PART_TIME")
         Mode mode,
+
+        @DefaultValue("HPF")
         Algorithm algorithm
 ) {
 }
