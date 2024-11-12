9788cae7c7c86a7bab077a5be861547a4db35603
geek <elmainiaymane03@gmail.com>
2024-11-12 22:15:49 +0100

    add visit already canceled exception

---
+++ src/main/java/com/wora/waitingRoom/waitingList/domain/exception/VisitAlreadyCanceledException.java
@@ -0,0 +1,7 @@
+package com.wora.waitingRoom.waitingList.domain.exception;
+
+public class VisitAlreadyCanceledException extends RuntimeException {
+    public VisitAlreadyCanceledException(String s) {
+        super(s);
+    }
+}
