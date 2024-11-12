c0cb9d62222a92eaf8248ab9851b94db3b693d0c
geek <elmainiaymane03@gmail.com>
2024-11-12 11:59:53 +0100

    fix bug: override value name of ids in visit Id

--- src/main/java/com/wora/waitingRoom/waitingList/domain/valueObject/VisitId.java
+++ src/main/java/com/wora/waitingRoom/waitingList/domain/valueObject/VisitId.java
@@ -1,8 +1,18 @@
 package com.wora.waitingRoom.waitingList.domain.valueObject;
 
+import com.wora.waitingRoom.visitor.domain.VisitorId;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.NotNull;
 
 @Embeddable
-public record VisitId(@GeneratedValue Long value) {
+public record VisitId(
+        @NotNull
+        @AttributeOverride(name = "value", column = @Column(name = "visitor_id"))
+        VisitorId visitorId,
+
+        @NotNull
+        @AttributeOverride(name = "value", column = @Column(name = "waiting_list_id"))
+        WaitingListId waitingListId) {
 }
