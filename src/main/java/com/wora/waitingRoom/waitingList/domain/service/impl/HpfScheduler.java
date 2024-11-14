01abd20d0efd567dc0cc7c45d17e6f1c7cccbc50
geek <elmainiaymane03@gmail.com>
2024-11-14 15:03:10 +0100

    finish the hpf algorithm implementation with tdd also and handle many cases
    handle if all visits have same priority so they will be ordered by
    arrival time
    
    handle case of given empty list, it was throwing another exceptions

--- src/main/java/com/wora/waitingRoom/waitingList/domain/service/impl/HpfScheduler.java
+++ src/main/java/com/wora/waitingRoom/waitingList/domain/service/impl/HpfScheduler.java
@@ -1,13 +1,25 @@
 package com.wora.waitingRoom.waitingList.domain.service.impl;
 
 import com.wora.waitingRoom.waitingList.domain.entity.Visit;
+import com.wora.waitingRoom.waitingList.domain.exception.MultipleWaitingListsFoundException;
 import com.wora.waitingRoom.waitingList.domain.service.Scheduler;
 
-import java.util.List;
+import java.util.*;
 
 public class HpfScheduler implements Scheduler {
     @Override
     public List<Visit> schedule(List<Visit> visits) {
-        return List.of();
+        if (visits.isEmpty())
+            return List.of();
+
+        if (areVisitsForSameWaitingList(visits))
+            throw new MultipleWaitingListsFoundException("Visits are not for the same waiting list");
+
+        return visits.stream()
+                .sorted(
+                        Comparator.comparing(Visit::getPriority)
+                                .thenComparing(Visit::getArrivalTime)
+                )
+                .toList();
     }
 }
---
+++ src/test/java/com/wora/waitingRoom/waitingList/domain/service/HpfSchedulerTest.java
@@ -0,0 +1,113 @@
+package com.wora.waitingRoom.waitingList.domain.service;
+
+import com.wora.waitingRoom.visitor.domain.Visitor;
+import com.wora.waitingRoom.waitingList.domain.entity.Visit;
+import com.wora.waitingRoom.waitingList.domain.entity.WaitingList;
+import com.wora.waitingRoom.waitingList.domain.exception.MultipleWaitingListsFoundException;
+import com.wora.waitingRoom.waitingList.domain.service.impl.HpfScheduler;
+import com.wora.waitingRoom.waitingList.domain.valueObject.Algorithm;
+import com.wora.waitingRoom.waitingList.domain.valueObject.WaitingListId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+
+@ExtendWith(SpringExtension.class)
+class HpfSchedulerTest {
+    private Scheduler underTest;
+    private List<Visit> visits;
+    private List<Visitor> visitors;
+    private WaitingList waitingList;
+
+    @BeforeEach
+    void setup() {
+        this.underTest = new HpfScheduler();
+        waitingList = WaitingList.builder()
+                .id(new WaitingListId(1L))
+                .date(LocalDate.now())
+                .algorithm(Algorithm.HPF)
+                .capacity(5)
+                .build();
+
+        visitors = List.of(
+                new Visitor("yahya", "el maini"),
+                new Visitor("abdelhak", "azrour"),
+                new Visitor("hamza", "lamin"),
+                new Visitor("soufiane", "bouanani"));
+    }
+
+    @Test
+    void givenVisitsDontBelongToSameWaitingList_whenSchedule_thenThrowMultipleWaitingListsFoundException() {
+        WaitingList otherWaitingList = WaitingList.builder()
+                .id(new WaitingListId(7L))
+                .date(LocalDate.now())
+                .algorithm(Algorithm.FIFO)
+                .capacity(8)
+                .build();
+
+        Visitor otherVisitor = new Visitor("abdelhak", "azrour");
+        visits = List.of(new Visit(visitors.getFirst(), waitingList, null, null),
+                new Visit(otherVisitor, otherWaitingList, null, null)
+        );
+
+        assertThatExceptionOfType(MultipleWaitingListsFoundException.class)
+                .isThrownBy(() -> underTest.schedule(visits))
+                .withMessage("Visits are not for the same waiting list");
+    }
+
+    @Test
+    void givenEmptyList_whenSchedule_thenReturnEmptyList() {
+        List<Visit> actual = underTest.schedule(List.of());
+
+        assertThat(actual.isEmpty()).isTrue();
+    }
+
+    @Test
+    void givenValidVisitsLists_whenSchedule_thenReturnOrderedVisitsByArrivalTime() {
+        visits = List.of(
+                new Visit(visitors.get(0), waitingList, (byte) 50, null),
+                new Visit(visitors.get(1), waitingList, (byte) 120, null),
+                new Visit(visitors.get(2), waitingList, (byte) 100, null),
+                new Visit(visitors.get(3), waitingList, (byte) 70, null)
+        );
+        List<Visit> actual = underTest.schedule(visits);
+
+        assertThat(actual.get(0).getPriority()).isEqualTo((byte) 50);
+        assertThat(actual.get(1).getPriority()).isEqualTo((byte) 70);
+        assertThat(actual.getLast().getPriority()).isEqualTo((byte) 120);
+    }
+
+    @Test
+    void givenVisitsWithSamePriority_whenSchedule_thenReturnVisitOrderedByArrivalTime() {
+        visits = List.of(
+                new Visit(visitors.get(0), waitingList, null, null),
+                new Visit(visitors.get(1), waitingList, null, null),
+                new Visit(visitors.get(2), waitingList, null, null),
+                new Visit(visitors.get(3), waitingList, null, null)
+        );
+        LocalTime firstOne = LocalTime.now().minusMinutes(4);
+        LocalTime secondOne = LocalTime.now().plusMinutes(11);
+        LocalTime lastOne = LocalTime.now().plusMinutes(40);
+
+        visits.get(0).setArrivalTime(firstOne);
+        visits.get(1).setArrivalTime(secondOne);
+        visits.get(2).setArrivalTime(lastOne);
+        visits.get(3).setArrivalTime(LocalTime.now().plusMinutes(30));
+
+        List<Visit> actual = underTest.schedule(visits);
+
+        assertThat(actual.get(0).getArrivalTime()).isEqualTo(firstOne);
+        assertThat(actual.get(1).getArrivalTime()).isEqualTo(secondOne);
+        assertThat(actual.getLast().getArrivalTime()).isEqualTo(lastOne);
+        assertThat(actual.get(0).getPriority()).isEqualTo((byte) 255);
+    }
+
+}
